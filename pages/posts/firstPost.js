e679c212e4a5e938f59c3ea34325949f4948c504
SalmonFillet <sy4.l3ucas@gmail.com>
2022-06-11 16:48:12 +0900

    ✨ Headコンポーネント

--- pages/posts/firstPost.js
+++ pages/posts/firstPost.js
@@ -1,8 +1,12 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default function FirstPost() {
   return (
     <div>
+      <Head>
+        <title>My First Post</title>
+      </Head>
       <h1>My First Post</h1>
       <Link href="/">back Home</Link>
     </div>
