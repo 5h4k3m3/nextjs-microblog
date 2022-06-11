eda87d0857eafe7f081bf6a337e4ba8989497e97
SalmonFillet <sy4.l3ucas@gmail.com>
2022-06-11 21:25:45 +0900

    ✨ css module

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,3 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import Link from 'next/link';
-
 import { Layout } from '../components/Layout';
 
 export default function Home() {
